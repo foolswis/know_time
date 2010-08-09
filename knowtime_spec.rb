acf22414ecf6b84986a496d7c7627c088e900f9f
Lloyd Budd <foolswisdom@gmail.com>
2010-08-08 17:27:00 -0700

    timer.minutes.should == 0 at start write 1st "assertion" (dont know term in rspec)

--- knowtime_spec.rb
+++ knowtime_spec.rb
@@ -1,4 +1,8 @@
-describe "KTTimer", "basic timing" do
-   it "should start with a time of 0"
+require "kt_timer"
+describe "KTTimer" do
+   it "should start with a time of 0" do
+      timer = KTTimer.new
+      timer.minutes.should == 0
+   end
    it "should return the time elapsed when stopped"
 end 
