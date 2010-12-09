17854c58126ed2a1aee0e7dde9b01b0469211f31
Lloyd Budd <foolswisdom@gmail.com>
2010-12-09 01:28:47 -0800

    refactor knowtime_spec.rb rm dup object setup

--- spec/knowtime/knowtime_spec.rb
+++ spec/knowtime/knowtime_spec.rb
@@ -3,21 +3,16 @@ require 'spec_helper'
 module KnowTime
   describe KnowTime do
     describe "#start" do
+      let(:output) { double('output').as_null_object }
+      let(:know_time) { KnowTime.new(output) }
+
       it "sends a welcome message" do
-        output = double('output').as_null_object
-        know_time = KnowTime.new(output)
-
         output.should_receive(:puts).with('Welcome to Know Time!')
-
         know_time.start
       end
 
       it "prompts to start recording" do
-        output = double('output').as_null_object
-        know_time = KnowTime.new(output)
-
         output.should_receive(:puts).with('Start recording? (y/n)')
-
         know_time.start
       end
     end
