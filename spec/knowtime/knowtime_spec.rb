1d894612c27468cc3efb9f2d20bb42ace32ecfff
Lloyd Budd <foolswisdom@gmail.com>
2010-12-09 01:14:15 -0800

    rspec for Welcome

--- spec/knowtime/knowtime_spec.rb
+++ spec/knowtime/knowtime_spec.rb
@@ -3,7 +3,15 @@ require 'spec_helper'
 module KnowTime
   describe KnowTime do
     describe "#start" do
-      it "sends a welcome message"
+      it "sends a welcome message" do
+        output = double('output')
+        know_time = KnowTime.new(output)
+
+        output.should_receive(:puts).with('Welcome to Know Time!')
+
+        know_time.start
+      end
+
       it "prompts to start recording"
     end
   end
