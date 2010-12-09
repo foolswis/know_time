0480a21a3b1930781982e38e1f7c60fde55b81e1
Lloyd Budd <foolswisdom@gmail.com>
2010-12-09 01:07:29 -0800

    rspec plumbing

---
+++ spec/knowtime/knowtime_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+module KnowTime
+  describe KnowTime do
+    describe "#start" do
+      it "sends a welcome message"
+      it "prompts to start recording"
+    end
+  end
+end
